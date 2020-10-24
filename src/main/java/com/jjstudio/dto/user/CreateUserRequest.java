b7255edd8e0d1ac417ef18a3e2592a6ad5c05df1
Justin Chung <justin.chung.zsl@gmail.com>
2020-10-24 12:05:43 -0700

    Remove username from create user request

--- src/main/java/com/jjstudio/dto/user/CreateUserRequest.java
+++ src/main/java/com/jjstudio/dto/user/CreateUserRequest.java
@@ -9,8 +9,6 @@ public class CreateUserRequest {
 
     private String lastName;
 
-    private String userName;
-
     public String getEmail() {
         return email;
     }
@@ -43,11 +41,4 @@ public class CreateUserRequest {
         this.lastName = lastName;
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
