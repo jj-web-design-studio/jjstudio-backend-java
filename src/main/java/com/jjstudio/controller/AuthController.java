ef9cb466222910fe08d3ee1bc4960a3478e92bbf
Justin Chung <justin.chung.zsl@gmail.com>
2020-11-29 22:51:53 -0800

    Add invalide JWT end point

--- src/main/java/com/jjstudio/controller/AuthController.java
+++ src/main/java/com/jjstudio/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.jjstudio.controller;
 
 import com.jjstudio.dto.AuthenticationRequest;
 import com.jjstudio.dto.AuthenticationResponse;
+import com.jjstudio.dto.InvalidateTokenRequest;
 import com.jjstudio.service.MyUserDetailsServiceImpl;
 import com.jjstudio.service.JwtUtil;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +30,7 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
-    @ApiOperation(value = "Get JWT", notes = "${AuthController.authenticatUser.notes}")
+    @ApiOperation(value = "Get JWT", notes = "${AuthController.authenticateUser.notes}")
     @PostMapping
     public ResponseEntity<AuthenticationResponse> authenticateUser(@RequestBody AuthenticationRequest request) throws Exception {
         try {
@@ -44,6 +46,16 @@ public class AuthController {
         final String jwt = jwtUtil.generateToken(userDetails);
 
         return new ResponseEntity<>(new AuthenticationResponse(jwt), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Invalidate JWT", notes = "${AuthController.invalidateToken.notes")
+    @PostMapping("/logout")
+    public ResponseEntity<String> invalidateToken(@RequestBody InvalidateTokenRequest request,
+                                                  Authentication authentication) {
+        // TODO: Invalidate user if JWT is valid
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        return new ResponseEntity<>("User has been logged out", HttpStatus.NO_CONTENT);
     }
 
 }
---
+++ src/main/java/com/jjstudio/dto/InvalidateTokenRequest.java
@@ -0,0 +1,13 @@
+package com.jjstudio.dto;
+
+public class InvalidateTokenRequest {
+    private String jwt;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
